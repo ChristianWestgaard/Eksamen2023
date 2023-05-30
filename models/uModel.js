2c1928214e0361238e832d103ea48161c8eade70
ChristianWestgaard <91060882+ChristianWestgaard@users.noreply.github.com>
2023-05-30 13:55:24 +0200

    Update uModel.js

--- models/uModel.js
+++ models/uModel.js
@@ -1,7 +1,8 @@
-const { log } = require('console')
+const { log, error } = require('console')
 const exp = require('constants')
 const mongoose = require('mongoose')
 const { isEmail } = require('validator')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -27,6 +28,24 @@ userSchema.post("save", function (doc, next){
     console.log("New user has been made", doc);
 })
 
+userSchema.pre("save", async function (next){
+    const pepper = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, pepper);
+    next();
+})
+
+userSchema.statics.login = async function (email,password) {
+    const user = await this.findOne({email: email});
+    if(user){
+        const auth = await bcrypt.compare(password, user.password)
+
+            if(auth){
+                return user;
+            }
+        throw Error("incorrect password")
+    }
+    throw Error("incorrect email")
+}
 
 const User = mongoose.model('user', userSchema);
 module.exports = User;
