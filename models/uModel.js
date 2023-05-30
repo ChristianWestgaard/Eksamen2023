2c8d72589737afe31952d7ec065aba06ea581151
ChristianWestgaard <91060882+ChristianWestgaard@users.noreply.github.com>
2023-05-30 10:56:53 +0200

    Optimized, server can run

--- models/uModel.js
+++ models/uModel.js
@@ -1,24 +1,32 @@
+const { log } = require('console')
 const exp = require('constants')
 const mongoose = require('mongoose')
-const { stringify } = require('querystring')
+const { isEmail } = require('validator')
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true,
+        required: [true, "Email is required"],
         unique: true,
-        lowercase: true
+        lowercase: true,
+        validate: [isEmail, "Enter a valid email"]
     },
     username: {
         type: String,
-        required: true,
+        required: [true, "Username is required"],
         unique: true,
     },
     password: {
-        required: true,
-        minlenght: 6
+        type: String,
+        required: [true, "Password is required"],
+        minlenght: [6, "Password must be at least 6 characters"]
     }
 })
+
+userSchema.post("save", function (doc, next){
+    console.log("New user has been made", doc);
+})
+
 
 const User = mongoose.model('user', userSchema);
 module.exports = User;
